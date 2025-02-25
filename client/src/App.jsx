bd1ada953b2e2706d8e4fb0fb389a78eea2ef635
Misho1019 <mikailtorres99@gmail.com>
2025-02-25 04:13:08 +0200

    Add useEffect and useState and fetch todo data

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -10,6 +10,10 @@ function App() {
         <h1>To-Do List</h1>
         <ToDoList />
       </div>
+
+      <footer className="footer">
+        <p>Copyright © designed by Mihail Todorov</p>
+      </footer>
     </>
   )
 }
--- client/src/components/ToDoList.jsx
+++ client/src/components/ToDoList.jsx
@@ -1,6 +1,20 @@
+import { useEffect, useState } from "react";
 import ToDoItem from "./ToDoItem";
 
 export default function ToDoList() {
+    const [todos, setTodos] = useState([]);
+
+    useEffect(() => {
+        fetch('http://localhost:3030/jsonstore/todos')
+            .then(res => res.json())
+            .then(data => {
+                const result = Object.values(data)
+                setTodos(result)
+            })
+            .catch(error => {
+                console.error(error.message);
+            })
+    }, [])
     return (
         <>
             <table>
@@ -12,7 +26,7 @@ export default function ToDoList() {
                     </tr>
                 </thead>
                 <tbody>
-                    <ToDoItem />
+                    {todos.map(todo => <ToDoItem />)}
                 </tbody>
             </table>
         </>
