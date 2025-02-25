57f82fff28c352375c7194356fb0489612e79a2d
Misho1019 <mikailtorres99@gmail.com>
2025-02-25 04:01:36 +0200

    Move ToDo table in ToDoList.jsx

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,5 +1,6 @@
 
 import './App.css'
+import ToDoList from './components/ToDoList'
 
 function App() {
 
@@ -7,34 +8,8 @@ function App() {
     <>
       <div class="container">
         <h1>To-Do List</h1>
-        <table>
-          <thead>
-            <tr>
-              <th>Task</th>
-              <th>Status</th>
-              <th>Action</th>
-            </tr>
-          </thead>
-          <tbody>
-            <tr>
-              <td>Finish project</td>
-              <td class="status pending">Pending</td>
-              <td><button class="toggle-btn">Change Status</button></td>
-            </tr>
-            <tr>
-              <td>Buy groceries</td>
-              <td class="status pending">Pending</td>
-              <td><button class="toggle-btn">Change Status</button></td>
-            </tr>
-            <tr>
-              <td>Read a book</td>
-              <td class="status completed">Completed</td>
-              <td><button class="toggle-btn">Change Status</button></td>
-            </tr>
-          </tbody>
-        </table>
+        <ToDoList />
       </div>
-
     </>
   )
 }
