0ce999ca5f32ff08fe572b158f71378d5aba37ea
Misho1019 <mikailtorres99@gmail.com>
2025-02-25 04:47:56 +0200

    Add dynamic status rendering

--- client/src/components/ToDoItem.jsx
+++ client/src/components/ToDoItem.jsx
@@ -1,11 +1,20 @@
+import styles from './ToDoItem.module.css'
+
 export default function ToDoItem({
     _id,
     task,
     status,
 }) {
+
+    const todoClassName = [styles['pending']]
+
+    if (status === 'Completed') {
+        todoClassName.push(styles['completed'])
+    }
+
     return (
         <>
-            <tr>
+            <tr className={todoClassName.join(' ')}>
                 <td>{task}</td>
                 <td class="status completed">{status}</td>
                 <td><button class="toggle-btn">Change Status</button></td>
